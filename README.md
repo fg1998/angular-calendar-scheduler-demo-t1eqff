8ab3cabade01c7987509d5947c41ab2068215e57
fg1998 <fg1998@gmail.com>
2024-11-07 20:59:36 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-calendar-scheduler-demo-t1eqff
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/fg1998/angular-calendar-scheduler-demo-t1eqff)
---
+++ src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { registerLocaleData } from '@angular/common';
+import localeIt from '@angular/common/locales/it';
+registerLocaleData(localeIt);
+
+import { AppComponent } from './app.component';
+
+import { CalendarModule, DateAdapter, MOMENT } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+
+import { SchedulerModule } from 'angular-calendar-scheduler';
+
+import { AppService } from './services/app.service';
+
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+import moment from 'moment';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    }),
+    SchedulerModule.forRoot({
+      locale: 'en',
+      headerDateFormat: 'daysRange',
+      logEnabled: true,
+    }),
+    MatProgressSpinnerModule,
+  ],
+  providers: [
+    AppService,
+    { provide: LOCALE_ID, useValue: 'en-US' },
+    { provide: MOMENT, useValue: moment },
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
